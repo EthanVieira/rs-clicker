6087ce0c00c8f7070ab2625b5456c716d3eef2e2
Will <redskinguy12@tamu.edu>
2018-07-31 22:52:52 -0500

    Started getting auto-resize to work

--- src/game.ts
+++ src/game.ts
@@ -7,8 +7,8 @@ import { MenuScene } from "./scenes/menuScene";
 const config: GameConfig = {
   title: "RS Clicker",
   version: "0.1",
-  width: window.innerWidth/1.5,
-  height: window.innerHeight/1.5,
+  width: window.innerWidth/1.2,
+  height: window.innerHeight/1.2,
   zoom: 1,
   type: Phaser.AUTO,
   parent: "game",
@@ -30,4 +30,25 @@ export class Game extends Phaser.Game {
 
 window.onload = () => {
   var game = new Game(config);
+  resize(game);
+  window.addEventListener("resize", function(){
+      resize(game);
+  }, false);
 };
+
+function resize(_game : Game) {
+  console.log("YO")
+  var canvas = document.querySelector("canvas");
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  var windowRatio = windowWidth / windowHeight;
+  var gameRatio = +_game.config.width / +_game.config.height;
+  if(windowRatio < gameRatio){
+      canvas.style.width = windowWidth + "px";
+      canvas.style.height = (windowWidth / gameRatio) + "px";
+  }
+  else{
+      canvas.style.width = (windowHeight * gameRatio) + "px";
+      canvas.style.height = windowHeight + "px";
+  }
+}
