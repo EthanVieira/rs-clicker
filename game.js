0bdc370b8669de8cd0e1c29674e9e1fc45dce987
greencur000 <greencur000@tamu.edu>
2018-08-24 23:27:09 -0500

    added objects

--- game.js
+++ game.js
@@ -1,5 +1,24 @@
 var game = new Phaser.Game(800, 550, Phaser.AUTO, '', {
-  preload: preload, create: create, update: update });
+  preload: preload, create: create, update: update 
+});
+
+// Upgrades
+var purchases = {
+	weapons: weapons,
+	spells: spells,
+	ranged: ranged
+}
+
+var spells = ["Air Strike", "Water Blast", "High Alc", "Earth Wave", "Blood Barrage", "Ice Barrage", "Fire Surge"];
+var weapons = ["Dagger", "Sword", "Halberd", "Battle Axe", "Scimitar"];
+var ranged = ["Shortbow", "Oak Shortbow", "Willow Shortbow", "Maple Shortbow", "Yew Shortbow", "Magic Shortbow", "Crystal Bow"];
+var ores = ["Bronze", "Iron", "Steel", "Mithril", "Adament", "Rune", "Dragon"];
+
+// Player
+var player = {
+	level: 1,
+	gold: 0
+}
 
 function preload() {
 
