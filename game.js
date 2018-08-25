6f7666af059941dc05392eb044c5d17e9c646139
greencur000 <greencur000@tamu.edu>
2018-08-25 15:01:56 -0500

    Changed to chicken

--- game.js
+++ game.js
@@ -17,19 +17,21 @@ var ores = ["Bronze", "Iron", "Steel", "Mithril", "Adament", "Rune", "Dragon"];
 // Player
 var player = {
 	level: 1,
-	gold: 0
+	gold: 0,
+	goldPerSec: 0
 }
 
 // Game objects
-var button;
-var background;
-var clicks = 0;
-var scoreText;
+var gameWindow = {
+	button: null,
+	background: null,
+	scoreText: null
+}
 
 function preload() {
 
   game.load.image('background', 'source/assets/background.png');
-  game.load.image('button', 'source/assets/logo.png');
+  game.load.image('button', 'source/assets/chicken.jpg');
 
   //game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   //game.scale.pageAlignVertically = true;
@@ -38,21 +40,25 @@ function preload() {
 
 function create() {
 
-  background = game.add.image(game.world.centerX, game.world.centerY, 'background');
-  background.anchor.setTo(0.5);
+	// Background
+	gameWindow.background = game.add.image(game.world.centerX, game.world.centerY, 'background');
+	gameWindow.background.anchor.setTo(0.5);
 
-  // Button
-  button = game.add.sprite(50, 50, 'button');
-  button.inputEnabled = true;
-  scoreText = game.add.text(16, 16, 'Clicks: 0', {fontSize: '32px', fill: '#555'});
-  button.events.onInputDown.add(listener, this);
+	// Button
+	gameWindow.button = game.add.sprite(50, 50, 'button');
+	gameWindow.button.inputEnabled = true;
+	gameWindow.scoreText = game.add.text(16, 16, 'Gold: 0', {fontSize: '32px', fill: '#999'});
+	gameWindow.button.events.onInputDown.add(buttonClick, this);
 
 }
 
-function listener()
-{
-  clicks++;
-  scoreText.text = 'Clicks: ' + clicks;
+function buttonClick(){
+	player.gold++;
+	gameWindow.scoreText.text = 'Gold: ' + player.gold;
+	gameWindow.button.tint = 0xff0000;
+	setTimeout(function(){
+		gameWindow.button.tint = 0xFFFFFF
+	}, 100);
 }
 
 /*function over(){
@@ -72,6 +78,4 @@ function actionOnClick(){
 	background.visible =! background.visible;
 } */
 
-function update() {
-
-}
+function update() {}
