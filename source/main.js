57c7c0d974019af7bfdd7e2dd02b25fefe8eec75
greencur000 <greencur000@tamu.edu>
2018-12-20 14:46:14 -0600

    Moved stuff into source

---
+++ source/main.js
@@ -0,0 +1,44 @@
+var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, 
+	window.innerHeight * window.devicePixelRatio, Phaser.AUTO, 'game'),
+	Main = function () {};
+
+Main.prototype = {
+
+	init: function () {
+		// Loading
+		this.status = game.make.text(game.world.centerX, game.world.centerY, 'loading...', {fill: 'white'});
+    this.status.anchor.setTo(0.5);
+		game.add.existing(this.status);
+	},
+
+	preload: function () {
+
+		// Load images
+		game.load.image('background', 'source/assets/MenuBg.png');
+		game.load.image('loading', 'source/assets/loading.png');
+		game.load.image('button', 'source/assets/chicken.jpg');
+		game.load.image('menu-bg', 'source/assets/background.png');
+
+		// Load states
+		game.load.script('Splash', 'splash.js');
+		game.load.script('Level1', 'level1.js');
+		game.load.script('Menu', 'menu.js');
+
+	},
+
+	create: function () {
+
+		// Add states
+		game.state.add('Splash', Splash);
+		game.state.add('Level1', Level1);
+		game.state.add('Menu', Menu);
+		game.state.start('Splash');
+		
+	}
+}
+
+// Start program
+game.state.add('Main', Main);
+game.state.start('Main');
+
+// function update() {}
