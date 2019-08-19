73ae18a2cd58ade3c197fba6dfccb47b3a43c5ca
Will <redskinguy12@tamu.edu>
2019-08-19 17:20:55 -0500

    Finished initial functionality for character creation

--- source/scenes/cc.js
+++ source/scenes/cc.js
@@ -8,51 +8,63 @@ export class CCScene extends Phaser.Scene{
     }
     init() {
         // receieve data from other scene that initialized this scene
+
+        // TODO: Adjust the starting positions of buttons/background by a set scale
+        // let scale = 1;
     }
     preload(){
-        // Image won't show up unless i preload it here as well?? Why do I need to do this again
-        this.load.image('cc-bg', 'source/assets/CCBg.png');
+        this.load.image('default', 'source/assets/cc_default.png');
+        this.load.image('warrior', 'source/assets/cc_warrior.png');
+        this.load.image('ranger', 'source/assets/cc_ranger.png');
+        this.load.image('mage', 'source/assets/cc_mage.png');
 
-        // Load buttons here I guess since loading in LoadScene is currently busted
-        // NEED:
-        // Play 
         this.load.image('play-button', 'source/assets/playbutton.png');
-        // Settings
-        // Stats
+        // TODO:
+            // Settings
+            // Stats
     }
     create(){
-        // create the menu screen
 
-        this.add.image(0, 0, 'cc-bg').setOrigin(0,0).setDepth(0);
+        // Load the default character creation screen
+        this.add.image(0, 0, 'default').setOrigin(0,0).setDepth(0);
 
-        // Button hover sprites
+        // Warrior Button
+        let warriorButton = this.add.image(130, 180, "play-button").setDepth(0);
+        warriorButton.setAlpha(0.01);
+        warriorButton.setInteractive();
 
+        // Mage Button
+        let mageButton = this.add.image(130, 290, "play-button").setDepth(0);
+        mageButton.setAlpha(0.01);
+        mageButton.setInteractive();
 
-        // Button format
-/*
-        let buttonName = this.add.image(locationX, locationY, "button-name").setDepth(1);
-        buttonName.setInteractive();
-        buttonName.on("pointerover", ()=>{
-             stuff on hover 
-        }
-        buttonName.on("pointerover", ()=>{
-             stuff on hover 
-        
-        }
-        buttonName.on("pointerout", ()=>{
-             stuff on not hover 
-        }
-        buttonName.on("pointerup", ()=>{
-             stuff on click 
-        }
-*/
-        // Buttons
-        let playButton = this.add.image(300, 300, "play-button").setDepth(1);
-        playButton.setInteractive();
-        
+        // Ranger Button
+        let rangerButton = this.add.image(130, 405, "play-button").setDepth(0);
+        rangerButton.setAlpha(0.01);
+        rangerButton.setInteractive();
+
+        // Accept Button
+        let acceptButton = this.add.image(400, 455, "play-button").setDepth(0);
+        acceptButton.setAlpha(0.01);
+        acceptButton.setInteractive();
+
+        warriorButton.on("pointerup", ()=>{
+            // Load the warrior image
+            this.add.image(0, 0, 'warrior').setOrigin(0,0).setDepth(0);
+        })
+
+        mageButton.on("pointerup", ()=>{
+            // Load the mage image
+            this.add.image(0, 0, 'mage').setOrigin(0,0).setDepth(0);
+        })
+
+        rangerButton.on("pointerup", ()=>{
+            // Load the ranger image
+            this.add.image(0, 0, 'ranger').setOrigin(0,0).setDepth(0);
+        })
 
         // Why isn't level one starting???
-        playButton.on("pointerup", ()=>{
+        acceptButton.on("pointerup", ()=>{
             this.scene.start(CONSTANTS.SCENES.LEVEL1); 
             console.log("Going to Level 1");
         })
