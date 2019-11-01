ca24b6ca2b379927b73ee60022981e506686a644
Will <redskinguy12@tamu.edu>
2019-11-01 10:30:42 -0500

    Added changing cash stack / gold text to shop

--- source/scenes/shop.js
+++ source/scenes/shop.js
@@ -28,19 +28,23 @@ export class Shop extends Phaser.Scene{
     preload(){
         // Background
         this.load.image('shop', 'source/assets/shopInterface.png');
+
+        // Cash Stacks
+        let stacks = ['5', '25', '100', '250', '1k', '10k'];
+        for(let i = 0; i < stacks.length; i++) {
+            this.load.image(stacks[i], 'source/assets/coin_stacks/' + stacks[i] + '_stack.png');
+        }
     }
 
     create(){
         // Background
-        this.shop = this.add.image(0, 0, 'shop').setOrigin(0,0).setDepth(0).setInteractive();
+        this.shop = this.add.image(0, 0, 'shop').setOrigin(0,0).setDepth(0)
 
         // Available Gold
-        // @TODO: Move this to line up with the gold icon
-        this.goldText = this.add.text(20, 13, 'Gold: ' + this.characterData.gold, {fill: 'white', fontSize: '28px'}).setDepth(3);
+        this.displayGold(this.characterData.gold);
 
         // Exit
-        // @TODO: Make this text invisible
-        this.exitButton = this.add.text(500, 0, 'exit').setInteractive();
+        this.exitButton = this.add.text(568, 15, 'exit').setDepth(-1).setInteractive();
         this.exitButton.on("pointerup", ()=>{
             // Pass in the current level to know which level to return to upon exiting the shop.
             this.scene.start(this.currentLevel, this.characterData);
@@ -53,7 +57,7 @@ export class Shop extends Phaser.Scene{
         //this.consumablesButton = ...
 
         // Load the array of available item objects (weapons by default)
-        this.loadItems("WEAPONS", this.characterData);
+        this.loadItems("WEAPONS", this.characterData.skills);
 
         // Display the items in the shop as interactive images
         this.loadingText = this.add.text(200, 200, 'Loading...', {fill: 'white', fontSize: '28px'}).setDepth(3);
@@ -73,38 +77,70 @@ export class Shop extends Phaser.Scene{
     //     }
     // }
 
+    // Outputs the player text in RS format: 1m = 1000k, 10m = green text, 1b = 1000m
+    displayGold(gold) {
+        // Pick gold image based on # of coins 
+        let stack = '';
+        switch(true) {
+            case (gold < 25):
+                stack = '5'; break;
+            case (gold < 100):
+                stack = '25'; break;
+            case (gold < 250):
+                stack = '100'; break;
+            case (gold < 1000):
+                stack = '250'; break;
+            case (gold < 10000):
+                stack = '1k'; break;
+            default:
+                stack = '10k'; break;
+        }
+        let goldImage = this.add.image(48, 346, stack).setInteractive();
+        goldImage.scale = 1.3;
+        // this.add.image(40, 320, stack).setInteractive();
+
+        // Pick text color and style based on # of coins
+        let color = 'white';
+        let goldText = this.characterData.gold;
+        if(gold > 99999 && gold < 10000000) {
+            goldText = (gold / 1000) + 'k';
+        }
+        else if(gold > 10000000) {
+            goldText = (gold / 1000000) + 'M';
+            fill = '#06c663';
+        }
+        this.add.text(33, 320, goldText, {fontFamily: '"runescape_uf"', fill: color, stroke: '#000000', strokeThickness: 2});
+    }
+
     // @TODO: Add armor as a potential item type? 
-    loadItems(itemType, characterData) {
+    loadItems(itemType, characterSkills) {
         switch(itemType) {
             case "WEAPONS":
-                this.loadWeapons(this.characterData);
-                break;
+                this.loadWeapons(characterSkills); break;
             case "TOOLS":
-                this.loadTools(this.characterData);
-                break;
+                this.loadTools(characterSkills); break;
             case "CONSUMABLES":
-                this.loadConsumables(this.characterData);
-                break;
+                this.loadConsumables(characterSkills); break;
             default:
                 console.log("Cannot Load Shop -- Invalid Item Type");
         }
     }
 
     // Load weapons based on the character's levels
-    loadWeapons(characterData) {
+    loadWeapons(characterSkills) {
         console.log("Loading Weapons");
         // Store the (x,y) coordinates for each image in the shop display
-        var imageX = {"attack": 100, "ranged": 100, "magic": 100};
-        var imageY = {"attack": 100, "ranged": 200, "magic": 300};
-        for(var weapon in ITEMS.WEAPONS) {
-            var weaponData = ITEMS.WEAPONS[weapon];
-            var requiredSkill = weaponData.skill;
-            var requiredLevel = characterData.skills[requiredSkill];
+        let imageX = {"attack": 100, "ranged": 100, "magic": 100};
+        let imageY = {"attack": 100, "ranged": 200, "magic": 300};
+        for(let weapon in ITEMS.WEAPONS) {
+            let weaponData = ITEMS.WEAPONS[weapon];
+            let requiredSkill = weaponData.skill;
+            let requiredLevel = characterSkills[requiredSkill];
             // Load the current weapon with the best possible material (e.g. dragon dagger)
-            var bestMat = this.getBestMat(requiredLevel, weaponData.material);
+            let bestMat = this.getBestMat(requiredLevel, weaponData.material);
             // Determine the corect (x,y) coordinate for the image to be displayed in the shop
-            var x = imageX[requiredSkill];
-            var y = imageY[requiredSkill];
+            let x = imageX[requiredSkill];
+            let y = imageY[requiredSkill];
             // Increment the x location for the next item of this type
             imageX[requiredSkill] += 100;
             this.shopItems.push(new Item(bestMat, weaponData, x, y));
@@ -112,18 +148,18 @@ export class Shop extends Phaser.Scene{
     }
 
     // @TODO
-    loadTools(characterData) {
+    loadTools(characterSkills) {
         console.log("Loading Tools");
     }
 
     // @TODO
-    loadConsumables(characterData) {
+    loadConsumables(characterSkills) {
         console.log("Loading Consumables");
     }
 
     // Load images dynamically to avoid having to preload every item image.
     loadImages() {
-        var item = this.shopItems[this.displayIndex++];
+        let item = this.shopItems[this.displayIndex++];
         console.log("Loading Image: ", item.name);
         this.displayX = item.x;
         this.displayY = item.y;
@@ -145,43 +181,50 @@ export class Shop extends Phaser.Scene{
         this.loadingText.visible = false;
         // Create icons for all of the loaded images
         for(let i = 0; i < this.shopItems.length; i++) {
-            var item = this.shopItems[i];
+            let item = this.shopItems[i];
             console.log("Displaying Item: ", item.name);
-            var tempIcon = this.add.image(item.x, item.y, item.name).setInteractive();
-            tempIcon.scale = .3;
+            let tempIcon = this.add.image(item.x, item.y, item.name).setInteractive();
+            tempIcon.scale = .25;
             // @TODO Add a real click-listener function that actually buys the item
             tempIcon.on("pointerup", ()=>{
                 this.buyItem(this.shopItems[i]);
             })
+            tempIcon.on("pointerover", ()=>{
+                this.displayToolTip(this.shopItems[i]);
+            })
             this.shopIcons.push(tempIcon);
         }
-        console.log("# Items: ", this.shopItems.length);
     }
 
-    // @TODO
+    // @TODO: Buy an item from the shop (add item to inventory / subtract from available gold)
     buyItem(item) {
         console.log("Buying Item:", item.name);
     }
 
-    // @TODO
-    updateGold() {
-        // If an item was bought, update the available gold and re-set the "itemBought" flag
+    // @TODO: Display the tooltip text for an item (item name / stats) when hovering over it
+    displayToolTip(item) {
+        console.log("Hovering Over Item: ", item.name);
     }
 
-    // @TODO
-    updateShop() {
-        // Check if an item was bought?
-    }
+    // // @TODO
+    // updateGold() {
+    //     // If an item was bought, update the available gold and re-set the "itemBought" flag
+    // }
+
+    // // @TODO
+    // updateShop() {
+    //     // Check if an item was bought?
+    // }
 
     //====== Helper Functions ======
 
     // Determine the best material type available for a character given their levels (e.g. 5 attack = steel weapons)
     getBestMat(reqLevel, matType) {
-        var bestMat;
+        let bestMat = 'No Mat';
         // Looping over values of this material type (e.g. SMITHINGMAT: Bronze, Iron, Steel, ...)
-        for(var mat in MATERIALS[matType]) {
+        for(let mat in MATERIALS[matType]) {
             // No dict.values() in Javascript, so this is a workaround. 
-            var matData = MATERIALS[matType][mat];
+            let matData = MATERIALS[matType][mat];
             if(matData.level > reqLevel)
                 break;
             bestMat = matData;
@@ -192,7 +235,7 @@ export class Shop extends Phaser.Scene{
 
 // Event listener for dynamically loading all of the images in the shopItems list.
 function addNextImage(key) {
-    var nextItem = this.shopItems[this.displayIndex++];
+    let nextItem = this.shopItems[this.displayIndex++];
 
     if(nextItem) {
         console.log("Loading Image: ", nextItem.name);
