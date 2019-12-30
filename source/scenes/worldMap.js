39a13a5b2d6988f0474aa1c0e58b9c22d8b5cf39
unknown <c.green5201@gmail.com>
2019-12-30 15:25:59 -0600

    World map cannot drag off screen, labels in correct location, and labels color when unlocked

--- source/scenes/worldMap.js
+++ source/scenes/worldMap.js
@@ -16,23 +16,36 @@ export class WorldMap extends Phaser.Scene{
         this.load.image('world-map', 'source/assets/WorldMap.png');
     }
     create(){
-        // Background
-        let map = this.add.image(0, 0, 'world-map').setOrigin(0,0).setDepth(0).setInteractive();
-        this.input.setDraggable(map);
-        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-            gameObject.x = dragX;
-            gameObject.y = dragY;
-        });
+    	let mapCenterX = -1000;
+    	let mapCenterY = -600;
+    	const mapWidth = 2161;
+    	const mapHeight = 1166;
+    	const screenWidth = 765;
+    	const screenHeight = 503;
 
-        // City text
-        let tutorialIsland = this.add.text(600, 360, 'Tutorial Island', {fill: 'white', fontSize: '20px'}).setDepth(1);
+        // Background
+        let map = this.add.image(0, 0, 'world-map').setOrigin(0,0).setDepth(0);
+
+        // Text
+        let fillColor = 'yellow';
+        // Color links if they haven't been unlocked yet
+        if (!this.characterData.TUTORIAL_ISLAND.questCompleted) {
+            fillColor = 'white';
+        }
+
+        // Tutorial Island
+        const tutorialIslandX = 1100;
+        const tutorialIslandY = 1070;
+        let tutorialIsland = this.add.text(tutorialIslandX, tutorialIslandY, 'Tutorial Island', {fill: 'yellow', fontSize: '20px', style: 'bold'}).setDepth(1);
         tutorialIsland.setInteractive();
         tutorialIsland.on('pointerup', ()=>{
             this.scene.start(CONSTANTS.SCENES.TUTORIAL_ISLAND, this.characterData); 
             console.log("Going to Tutorial Island");   
         })
 
-        let lumbridge = this.add.text(600, 390, 'Lumbridge', {fill: 'white', fontSize: '20px'}).setDepth(1);
+        const lumbridgeX = 1400;
+        const lumbridgeY = 815;
+        let lumbridge = this.add.text(lumbridgeX, lumbridgeY, 'Lumbridge', {fill: fillColor, fontSize: '20px', style: 'bold'}).setDepth(1);
         lumbridge.setInteractive();
         lumbridge.on('pointerup', ()=>{
             if (this.characterData.TUTORIAL_ISLAND.questCompleted) {
@@ -45,12 +58,38 @@ export class WorldMap extends Phaser.Scene{
         })
 
         // Trees
-        let lumbridgeTrees = this.add.text(600, 420, 'Lumbridge Trees', {fill: 'white', fontSize: '20px'}).setDepth(1);
+        const lumbridgeTreesX = 1280;
+        const lumbridgeTreesY = 800;
+        let lumbridgeTrees = this.add.text(lumbridgeTreesX, lumbridgeTreesY, 'Lumbridge\nTrees', {fill: fillColor, fontSize: '20px', style: 'bold'}).setDepth(1);
         lumbridgeTrees.setInteractive();
         lumbridgeTrees.on('pointerup', ()=>{
-            this.scene.start(CONSTANTS.SCENES.LUMBRIDGE_TREES, this.characterData); 
-            console.log("Going to Lumbridge Trees");   
+        	if (this.characterData.TUTORIAL_ISLAND.questCompleted) {
+	            this.scene.start(CONSTANTS.SCENES.LUMBRIDGE_TREES, this.characterData); 
+	            console.log("Going to Lumbridge Trees");  
+	        }
+	        else {
+                console.log("Lumbridge not unlocked yet");
+            }
         })
+
+        // Group objects together
+        let container = this.add.container(mapCenterX, mapCenterY);
+        container.add(map);
+        container.add(tutorialIsland);
+        container.add(lumbridge);
+        container.add(lumbridgeTrees);
+
+        // Setup drag limits
+        container.setInteractive(new Phaser.Geom.Rectangle(0, 0, mapWidth, mapHeight), Phaser.Geom.Rectangle.Contains); // Use size of map image
+		this.input.setDraggable(container);
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+        	if (mapWidth - screenWidth + dragX > 0 && dragX < 0) {
+				gameObject.x = dragX;
+        	}
+            if (mapHeight - screenHeight + dragY > 0 && dragY < 0) {
+				gameObject.y = dragY;
+        	}
+        });
     }
 }
 
