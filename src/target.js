11abeb49cc3263ffa476fe16b8c46bc3223cfd8f
unknown <c.green5201@gmail.com>
2020-04-09 19:49:04 -0500

    Formatting

--- src/target.js
+++ src/target.js
@@ -1,6 +1,6 @@
 export class Target {
     target;
-	progressBar;
+    progressBar;
     name = "";
     drops = [];
 
@@ -15,17 +15,17 @@ export class Target {
             .setOrigin(0.5, 0)
             .setDepth(2)
             .setScale(.4)
-			.setInteractive()
-			.on("pointerup", () => {
-				// Check if it is the current target
-				if (this.target.visible) {
-					this.clickTarget();
-				}
-			});
-		this.target.visible = false;
+            .setInteractive()
+            .on("pointerup", () => {
+                // Check if it is the current target
+                if (this.target.visible) {
+                    this.clickTarget();
+                }
+            });
+        this.target.visible = false;
 
         // Set other vars
-		this.name = data.name;
+        this.name = data.name;
         this.drops = data.drops;
         this.scene = data.scene;
         this.stats = data.scene.stats;
@@ -33,12 +33,12 @@ export class Target {
     }
 
     clickTarget() {
-		// Target click interaction to be implemented by the child
-		let progress = this.getClickValue();
-		this.onClick(progress);
-		
-		// Increase progress and check status
-		this.updateProgress(progress);
+        // Target click interaction to be implemented by the child
+        let progress = this.getClickValue();
+        this.onClick(progress);
+        
+        // Increase progress and check status
+        this.updateProgress(progress);
 
         // Log click for stats
         this.stats.updateClickedTargetStat();
@@ -56,12 +56,12 @@ export class Target {
                     this.scene.dashboard.inventory.obj.addToInventory(droppedItem);
                 }
             });
-			
-			// Check for target specific actions
-			this.onCompletion();
-			
-			// Show next target
-			this.scene.showRandomClickObject();
+            
+            // Check for target specific actions
+            this.onCompletion();
+            
+            // Show next target
+            this.scene.showRandomClickObject();
         }
     }
 
