1925ec6579469344a0b06b26d4d61165a6e72e09
unknown <c.green5201@gmail.com>
2020-04-26 17:27:54 -0500

    Re-added clickable object class

---
+++ src/clickable-object.js
@@ -0,0 +1,87 @@
+import { CONSTANTS, FONTS } from "./constants/constants.js";
+
+export class ClickableObject {
+    // Basic info
+    examineText = "";
+    
+    // Objects
+    menu;
+    scene;
+
+    // Right click actions
+    actions = [{ text: "Examine", func: "examine" }];
+
+    examine() {
+        console.log(this.examineText);
+        this.dashboard.showObjectInfo(true, this);
+    }
+
+    createRightClickMenu(x, y) {
+        let menuBox = this.scene.add
+            .image(x, y, "right-click-menu")
+            .setDepth(4)
+            .setInteractive()
+            .on("pointerout", pointer => {
+                // Check to ensure it doesn't trigger when hovering over text options
+                if (
+                    pointer.worldX > x + menuBox.width / 2 ||
+                    pointer.worldY > y + menuBox.height / 2 ||
+                    pointer.worldX < x - menuBox.width / 2 ||
+                    pointer.worldY < y - menuBox.height / 2
+                ) {
+                    this.menu.destroy();
+                }
+            });
+
+        // Get dashboard scene
+        this.dashboard = this.scene.scene.get(CONSTANTS.SCENES.DASHBOARD);
+
+        // Show stats menu until right click menu is destroyed
+        menuBox.on("destroy", () => {
+            this.dashboard.showObjectInfo(false);
+        });
+
+        // Add text options
+        // Have to use two separate texts per option for different colors
+        let options = [menuBox];
+        let optionsY = y - 45;
+
+        // Generate dynamic list of actions (wield, bury, etc.)
+        this.actions.forEach(action => {
+            optionsY += 15;
+            let itemText = this.scene.add.text(
+                x - 20,
+                optionsY,
+                this.name,
+                FONTS.ITEM_NAME
+            );
+            let actionText = this.scene.add
+                .text(x - 78, optionsY, action.text, FONTS.OPTIONS_MENU)
+                .setInteractive()
+                .setDepth(5)
+                .on("pointerdown", () => {
+                    this[action.func]();
+
+                    // Keep menu up when examining
+                    if (action.text != "Examine") {
+                        this.menu.destroy();
+                    }
+                });
+
+            options.push(actionText);
+            options.push(itemText);
+        });
+
+        let cancelText = this.scene.add
+            .text(x - 78, optionsY + 15, "Cancel", FONTS.OPTIONS_MENU)
+            .setInteractive()
+            .on("pointerdown", () => {
+                console.log("cancel");
+                this.menu.destroy();
+            });
+        options.push(cancelText);
+
+        // Group objects together
+        this.menu = this.scene.add.container(0, 0, options).setDepth(5);
+    }
+}
