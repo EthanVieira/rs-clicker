e6137c496e2599899c565a7e5ab405315c591787
CurtisGreen <c.green5201@gmail.com>
2020-05-05 17:21:16 -0500

    Fixed dumb mistake

--- src/ui/health-bar.js
+++ src/ui/health-bar.js
@@ -10,7 +10,7 @@ export class HealthBar {
     constructor(scene, x, y, maxHealth) {
         // Offset coordinates based on width/height of health bar
         let baseWidth = 80;
-        this.width = maxHealth + 80;
+        this.width = maxHealth + baseWidth;
         this.height = 14;
         this.x = x - 4 - (5 * this.width) / 12;
         this.y = y - this.height;
