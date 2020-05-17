cce168691066abf6c3cf1db13d07fc4c51da6e21
greencur000 <greencur000@tamu.edu>
2020-05-17 13:24:41 -0500

    Scrollbar can now be dragged. Top/bot buttons can be used as well.

--- src/ui/scroll-window.js
+++ src/ui/scroll-window.js
@@ -15,6 +15,7 @@ export class ScrollWindow extends Phaser.Scene {
 
     listHeight = 0;
     listDelta = 30;
+    curDelta = 0;
 
     // Images
     scrollHeader;
@@ -54,18 +55,23 @@ export class ScrollWindow extends Phaser.Scene {
             this.scrollBackground.destroy();
             this.scrollButton.destroy();
         }
+        if (this.timer != undefined) {
+            this.timer.remove();
+        }
 
         // Add scroll bar images
         this.scrollHeader = this.add
             .image(data.width, 0, "scroll-header")
             .setDepth(4)
-            .setOrigin(0, 0);
+            .setOrigin(0, 0)
+            .setInteractive();
         let scrollHeaderHeight = this.scrollHeader.displayHeight;
 
         this.scrollFooter = this.add
             .image(data.width, data.height - scrollHeaderHeight, "scroll-footer")
             .setDepth(4)
-            .setOrigin(0, 0);
+            .setOrigin(0, 0)
+            .setInteractive();
 
         this.scrollBackground = this.add
             .image(data.width, scrollHeaderHeight, "scroll-background")
@@ -75,7 +81,8 @@ export class ScrollWindow extends Phaser.Scene {
         this.scrollButton = this.add
             .image(data.width, scrollHeaderHeight, "scroll-button")
             .setDepth(4)
-            .setOrigin(0, 0);
+            .setOrigin(0, 0)
+            .setInteractive();
 
         // Set default scaling based on height input, may change below based on list height
         this.scrollButton.displayHeight =
@@ -116,7 +123,6 @@ export class ScrollWindow extends Phaser.Scene {
                 this.scrollButton.scaleY = data.height / this.listHeight;
                 this.scrollBackground.displayHeight =
                     data.height - this.scrollHeader.displayHeight * 2;
-                let scrollButtonHeight = this.scrollButton.displayHeight;
 
                 // Make scroll button evenly divisible so it will fit properly at the top/bottom
                 let remainingListDist = this.listHeight - data.height;
@@ -130,32 +136,44 @@ export class ScrollWindow extends Phaser.Scene {
 
                 // Move scroll button & objects
                 this.input.on("wheel", (pointer, gameObjects, deltaX, deltaY, deltaZ) => {
-                    if (this.listHeight > this.data.height) {
-                        if (deltaY > 0) {
-                            deltaY = this.listDelta;
-                        } else if (deltaY < 0) {
-                            deltaY = this.listDelta * -1;
-                        }
+                    this.scroll(deltaY, remainingScrollBarDist, remainingListDist);
+                });
 
-                        // Scale scroll bar delta
-                        let scrollBarDelta =
-                            deltaY * (remainingScrollBarDist / remainingListDist);
+                // Setup timer to check if scrollheader/footer is being held
+                this.timer = this.time.addEvent({
+                    delay: 100,                // ms
+                    callback: () => {
+                        this.scroll(this.curDelta, remainingScrollBarDist, remainingListDist);
+                    },
+                    loop: true,
+                    paused: true,
+                });
 
-                        // Check top/bottom bounds
-                        let upperBound = this.scrollButton.y + scrollBarDelta;
-                        let lowerBound = Math.floor(
-                            this.scrollButton.y + scrollButtonHeight + scrollBarDelta
-                        );
-                        if (
-                            (scrollBarDelta < 0 && upperBound >= scrollHeaderHeight) ||
-                            (scrollBarDelta > 0 && lowerBound <= this.scrollFooter.y)
-                        ) {
-                            // Scroll objects
-                            this.scrollButton.y += scrollBarDelta;
-                            data.objects.forEach((object) => {
-                                object.setY(object.y - deltaY);
-                            });
-                        }
+                this.scrollHeader.on("pointerdown", () => {
+                    this.scroll(-1, remainingScrollBarDist, remainingListDist);
+                    this.curDelta = -1;
+                    this.timer.paused = false;
+                });
+                this.scrollHeader.on("pointerup", () => {
+                    this.timer.paused = true;
+                    this.timer.elapsed = 0;
+                });
+
+                this.scrollFooter.on("pointerdown", () => {
+                    this.scroll(1, remainingScrollBarDist, remainingListDist);
+                    this.curDelta = 1;
+                    this.timer.paused = false;
+                });
+                this.scrollFooter.on("pointerup", () => {
+                    this.timer.paused = true;
+                    this.timer.elapsed = 0;
+                });
+
+                this.input.setDraggable(this.scrollButton);
+                this.input.on("drag", (pointer, gameObject, dragX, dragY) => {
+                    let drag = dragY - this.scrollButton.y;
+                    if (Math.abs(drag) > 5) {
+                        this.scroll(dragY - this.scrollButton.y, remainingScrollBarDist, remainingListDist);
                     }
                 });
             }
@@ -167,6 +185,38 @@ export class ScrollWindow extends Phaser.Scene {
                 data.width + xInit,
                 data.height
             );
+        }
+    }
+
+    scroll(deltaY, remainingScrollBarDist, remainingListDist) {
+        if (this.listHeight > this.data.height) {
+            if (deltaY > 0) {
+                deltaY = this.listDelta;
+            } else if (deltaY < 0) {
+                deltaY = this.listDelta * -1;
+            }
+
+            // Scale scroll bar delta
+            let scrollBarDelta =
+                deltaY * (remainingScrollBarDist / remainingListDist);
+
+            // Check top/bottom bounds
+            let scrollButtonHeight = this.scrollButton.displayHeight;
+            let scrollHeaderHeight = this.scrollHeader.displayHeight;
+            let upperBound = this.scrollButton.y + scrollBarDelta;
+            let lowerBound = Math.floor(
+                this.scrollButton.y + scrollButtonHeight + scrollBarDelta
+            );
+            if (
+                (scrollBarDelta < 0 && upperBound >= scrollHeaderHeight) ||
+                (scrollBarDelta > 0 && lowerBound <= this.scrollFooter.y)
+            ) {
+                // Scroll objects
+                this.scrollButton.y += scrollBarDelta;
+                this.data.objects.forEach((object) => {
+                    object.setY(object.y - deltaY);
+                });
+            }
         }
     }
 
