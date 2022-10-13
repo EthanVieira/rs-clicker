63e4959cb7f142ca6c739d0f394f54164a7a8df7
Ethan Vieira <ethanvieira@gmail.com>
2022-10-12 23:03:56 -0500

    last commit didn't add log removal

--- src/data/test-data.js
+++ src/data/test-data.js
@@ -29,8 +29,6 @@ smithingTestData["inventory"].filter((x) => x.item == "Coin")[0].count = 69240;
 smithingTestData["inventory"].push({ item: "BronzeBar", count: 42069 });
 smithingTestData["inventory"].push({ item: "CopperOre", count: 76543 });
 smithingTestData["inventory"].push({ item: "TinOre", count: 76543 });
-
-console.log(smithingTestData["inventory"]);
 // ---------------------------------------------------------------------
 export const dataMap = {
     "new-game": defaultData,
